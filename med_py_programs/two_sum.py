6a7fb2fb77f24c09444784f1078c43b65dc6e148
lukedevfrancis@gmail.com <lukedevfrancis@gmail.com>
2023-04-05 15:55:29 -0400

    made function

--- med_py_programs/two_sum.py
+++ med_py_programs/two_sum.py
@@ -1,19 +1,17 @@
 
-# return indices of the two numbers such that they add up to target.
+# returns indices of the two numbers such that they add up to target.
 
-target = 9
-def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
+target = 6
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    for i, num1 in enumerate(nums):
+        for j, num2 in enumerate(nums[i + 1:]):
+            if num1 + num2 == target:
+                return  ('[' + str(i) + ', ' + str(i+j+1) + ']')
 
-        for i, num1 in enumerate(nums):
-            for j, num2 in enumerate(nums[i + 1:]):
-                if num1 + num2 == target:
-                    print('[' + str(i) + ', ' + str(i+j+1) + ']')
-
-nums = [2, 7, 11, 15]
-twoSum(nums, target)
+nums = [3, 2, 4]
+print(twoSum(nums, target))
