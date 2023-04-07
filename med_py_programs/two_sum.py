86eb9529dc04a8c1baaaee33187656bf12b4e2bb
lukedevfrancis@gmail.com <lukedevfrancis@gmail.com>
2023-04-07 15:48:38 -0400

    moved variable inside function

--- med_py_programs/two_sum.py
+++ med_py_programs/two_sum.py
@@ -1,13 +1,14 @@
 
 # returns indices of the two numbers such that they add up to target.
 
-target = 6
-def twoSum(nums, target):
+def twoSum(nums, target):   
     """
     :type nums: List[int]
     :type target: int
     :rtype: List[int]
     """
+    target = 6
+
     for i, num1 in enumerate(nums):
         for j, num2 in enumerate(nums[i + 1:]):
             if num1 + num2 == target:
